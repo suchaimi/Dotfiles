d3da265715599ac5c77474b6b94764d4f6fd31bd
suchaimi <suchaimi.afif@gmail.com>
2019-11-16 03:34:20 +0700

    My dotfiles

---
+++ .config/glava/bars.glsl
@@ -0,0 +1,20 @@
+
+/* center line thickness (pixels) */
+#define C_LINE 1
+
+/* width (in pixels) of each bar */
+#define BAR_WIDTH 10
+/* width (in pixels) of each bar gap */
+#define BAR_GAP 2
+/* outline color */
+#define BAR_OUTLINE #262626
+/* outline width (in pixels, set to 0 to disable outline drawing) */
+#define BAR_OUTLINE_WIDTH 0
+/* Amplify magnitude of the results each bar displays */
+#define AMPLIFY 400
+/* Bar color */
+#define COLOR (#5C738B * ((d / 60) + 1))
+/* Direction that the bars are facing, 0 for inward, 1 for outward */
+#define DIRECTION 1
+/* Whether to switch left/right audio buffers */
+#define INVERT 1
---
+++ .config/spotify/Users/pvf3hs69liy8gs9q1xmuna0va-user/frecency.pb
@@ -0,0 +1,5 @@
+
+;
+$spotify:album:3THom8qoWFJIQZ8oUHU2uX	«ζΝΚί1w@ –‘ύν
+;
+$spotify:album:7ayBZIe1FHkNv0T5xFCX6F	υ0ψΊί1w@ Φύν
